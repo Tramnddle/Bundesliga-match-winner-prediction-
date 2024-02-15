57944db98857228257deb0f25cd78f8c6965184a
Tram <tram.nguyen.ngoc.nddle@gmail.com>
2024-02-15 17:46:30 +0100

    Update script_st.py

--- script_st.py
+++ script_st.py
@@ -179,7 +179,7 @@ date = user_inputs_date
 historical_matches_1 = matches_rolling[(matches_rolling['team'] == away_team) & (matches_rolling['opponent'] == home_team)] # historical matches stats of opponent team
 historical_matches_2 = matches_rolling[(matches_rolling['team'] == home_team) & (matches_rolling['opponent'] == away_team)] # historical matches stats of home team
 st.write(historical_matches_1['date'].dtype)
-st.write(user_inputs_date.dtype)
+st.write(type(user_inputs_date))
         # Exclude the current match by filtering based on the date
 historical_matches_1 = historical_matches_1[historical_matches_1['date'] < date]
 historical_matches_2 = historical_matches_2[historical_matches_2['date'] < date]
