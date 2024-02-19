c1f484ba068fa0f7f77d4e3a3605424efc1dd861
Tram <tram.nguyen.ngoc.nddle@gmail.com>
2024-02-19 19:06:52 +0100

    Update script_st.py

--- script_st.py
+++ script_st.py
@@ -279,13 +279,13 @@ match_AB = match_AB.set_index('date', inplace=False)
 st.dataframe(match_AB)
 
 match_BA = pd.DataFrame(matches_rolling[(matches_rolling['team']==user_inputs_B)&(matches_rolling['opponent']==user_inputs_A)&(matches_rolling['date']==date)])
-#match_BA[['save%_rolling_opp', 'gf_rolling_opp', 'gf_hist_opp', 'poss_hist_opp', 'gf_hist_home', 'poss_hist_home']] = match_AB[['save%_rolling', 'gf_rolling', 'gf_hist_home', 'poss_hist_home', 'gf_hist_opp', 'poss_hist_opp']]
-match_BA['save%_rolling_opp'] = match_AB['save%_rolling'][0]
-match_BA['gf_rolling_opp'] = match_AB['gf_rolling'][0]
-match_BA['gf_hist_opp'] = historical_data_2['gf']
-match_BA['poss_hist_opp'] = historical_data_2['poss']
-match_BA['gf_hist_home'] = historical_data_1['gf']
-match_BA['poss_hist_home'] = historical_data_1['poss']
+match_BA[['save%_rolling_opp', 'gf_rolling_opp', 'gf_hist_opp', 'poss_hist_opp', 'gf_hist_home', 'poss_hist_home']] = match_AB[['save%_rolling', 'gf_rolling', 'gf_hist_home', 'poss_hist_home', 'gf_hist_opp', 'poss_hist_opp']].iloc[0]
+#match_BA['save%_rolling_opp'] = match_AB['save%_rolling'][0]
+#match_BA['gf_rolling_opp'] = match_AB['gf_rolling'][0]
+#match_BA['gf_hist_opp'] = historical_data_2['gf']
+#match_BA['poss_hist_opp'] = historical_data_2['poss']
+#match_BA['gf_hist_home'] = historical_data_1['gf']
+#match_BA['poss_hist_home'] = historical_data_1['poss']
 match_BA['save%_hist_opp']= historical_data_2['save%']
 match_BA = match_BA[['date','round', 'gf_rolling','ga_rolling','sh_rolling', 'save%_rolling', 'poss_rolling',
                         'average_gf_s', 'average_gf_sr',  'average_gf_r', 'average_gf_t','average_ga_t','average_gf_st','average_ga_st',
